75473b69efcad2bc9bea70cce9ac7364f2566da1
eylulgokce <44869816+eylulgokce@users.noreply.github.com>
2020-05-16 17:17:39 +0300

    Frame for fragments added

--- Source/FindMeASeat/app/src/main/java/at/ac/univie/hci/findmeaseat/HomeActivity.java
+++ Source/FindMeASeat/app/src/main/java/at/ac/univie/hci/findmeaseat/HomeActivity.java
@@ -1,30 +1,43 @@
 package at.ac.univie.hci.findmeaseat;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.ui.AppBarConfiguration;
 
 import at.ac.univie.hci.findmeaseat.ui.bookings.BookingFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
 
+    public static FragmentManager fragmentManager_booking;
+    public static FragmentManager fragmentManager_building;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.home_activity);
-        BookingFragment bookingFragment = new BookingFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.home_booking_frame, bookingFragment).commit();
+
+        fragmentManager_booking = getSupportFragmentManager();
+
+        if(findViewById(R.id.home_booking_fragment_frame) != null){
+            if(savedInstanceState != null){
+                return;
+            }
+            FragmentTransaction fragmentTransaction_booking = fragmentManager_booking.beginTransaction();
+            BookingFragment bookingFragment = new BookingFragment();
+            fragmentTransaction_booking.add(R.id.home_booking_fragment_frame, bookingFragment, null);
+            fragmentTransaction_booking.commit();
+        }
+
 
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications).build();
-        
+
+
+
     }
 
 
