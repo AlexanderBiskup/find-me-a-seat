85074fc2946182d5e86fc8a16a605404899165b5
Alexander Biskup <alexander.biskup.at@gmail.com>
2020-05-16 11:53:57 +0200

    add cancel booking button

--- Source/FindMeASeat/app/src/main/java/at/ac/univie/hci/findmeaseat/ui/bookings/BookingDetailsActivity.java
+++ Source/FindMeASeat/app/src/main/java/at/ac/univie/hci/findmeaseat/ui/bookings/BookingDetailsActivity.java
@@ -1,9 +1,12 @@
 package at.ac.univie.hci.findmeaseat.ui.bookings;
 
 import android.os.Bundle;
+import android.view.ViewManager;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import at.ac.univie.hci.findmeaseat.R;
@@ -23,10 +26,19 @@ public class BookingDetailsActivity extends AppCompatActivity implements Booking
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking_details);
         UUID bookingId = UUID.fromString(getIntent().getStringExtra(BOOKING_ID_EXTRA_NAME));
+        Booking booking = bookingService.getBooking(bookingId);
         setTitle("Buchungsdetails");
         if (findViewById(R.id.booking_details_booking_container) != null) {
             BookingFragment fragment = BookingFragment.newInstance(bookingId);
             getSupportFragmentManager().beginTransaction().add(R.id.booking_details_booking_container, fragment).commit();
+        }
+        Button cancelBookingButton = findViewById(R.id.cancelBookingButton);
+        if(LocalDateTime.now().isBefore(booking.getStart())) {
+            cancelBookingButton.setText("Buchung stornieren");
+        } else if(LocalDateTime.now().isBefore(booking.getEnd())) {
+            cancelBookingButton.setText("Buchung beenden");
+        } else {
+            ((ViewManager) getParent()).removeView(cancelBookingButton);
         }
     }
 
