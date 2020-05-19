35ed4bd79cba41c227500842b5dfa1285e03b4da
hassana88 <a11807094@univie.ac.at>
2020-05-19 16:04:57 +0200

    bugfix(refresh home after booking and favorites get deleted)

--- Source/FindMeASeat/app/src/main/java/at/ac/univie/hci/findmeaseat/ui/home/HomeFragment.java
+++ Source/FindMeASeat/app/src/main/java/at/ac/univie/hci/findmeaseat/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package at.ac.univie.hci.findmeaseat.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,12 +9,43 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import java.util.Objects;
+
 import at.ac.univie.hci.findmeaseat.R;
 
 public class HomeFragment extends Fragment {
+    private boolean refreshOnResume = false;
+
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+        return fragment;
+    }
+
+    public HomeFragment() { }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(refreshOnResume){
+            getActivity().finish();
+            startActivity(getActivity().getIntent());
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        refreshOnResume = true;
     }
 
 }
