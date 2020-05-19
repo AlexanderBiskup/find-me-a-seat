0ec139622c3b594b3f095a6cbc996c6777f33dd5
hassana88 <a11807094@univie.ac.at>
2020-05-19 18:30:56 +0200

    landscape deactivated

--- Source/FindMeASeat/app/src/main/java/at/ac/univie/hci/findmeaseat/ui/home/HomeFragment.java
+++ Source/FindMeASeat/app/src/main/java/at/ac/univie/hci/findmeaseat/ui/home/HomeFragment.java
@@ -1,6 +1,5 @@
 package at.ac.univie.hci.findmeaseat.ui.home;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,17 +8,10 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import java.util.Objects;
-
 import at.ac.univie.hci.findmeaseat.R;
 
 public class HomeFragment extends Fragment {
     private boolean refreshOnResume = false;
-
-    public static HomeFragment newInstance() {
-        HomeFragment fragment = new HomeFragment();
-        return fragment;
-    }
 
     public HomeFragment() { }
 
